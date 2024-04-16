ff6eb94c1ab8648dee5c7ceaf2b0263c8bf840a7
flexagoon <code@fxgn.dev>
2024-04-17 01:21:10 +0300

    fix: properly serialize forwarded entity name

--- serialization/serialization.py
+++ serialization/serialization.py
@@ -48,9 +48,13 @@ async def serialize(message: Message) -> dict[str, Any]:
         data |= await __serialize_peer(message.client, message.from_id, "from")
 
         if forward := message.fwd_from:
-            data["forwarded_from"] = (
-                forward.from_name or await message.client.get_peer_id(forward.from_id)
+            entity = forward.from_name or await message.client.get_entity(
+                forward.from_id,
             )
+            if hasattr(entity, "first_name"):
+                data["forwarded_from"] = entity.first_name or entity.id
+            else:
+                data["forwarded_from"] = entity.title or entity.id
 
         if message.reply_to and hasattr(message.reply_to, "reply_to_msg_id"):
             data |= __serialize_reply(message)
